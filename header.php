5c94fb24522ae7a4579a81eeb9186c6de59f1f98
JorgeQZ <jorge_qzg@hotmail.com>
2024-11-22 09:55:04 -0600

    single - vacantes / postualciones

--- header.php
+++ header.php
@@ -1,16 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
+
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo(); ?></title>
     <?php wp_head(); ?>
 </head>
+
+
 <body>
 
 <header>
-    <!-- <?php
-    wp_nav_menu( 'primary_menu' );
-    ?> -->
+    <div>
+        <div class="container">
+            <?php
+
+            $custom_logo_id = get_theme_mod( 'custom_logo' );
+            $logo = wp_get_attachment_image_src( $custom_logo_id, 'full' );
+            if ( has_custom_logo() ): ?>
+            <div class="site-info">
+                <img class="main-logo" src="<?php echo esc_url( $logo[0] ) ?>" alt="<?php echo get_bloginfo( 'name' ) ?>'">
+                <div class="site-title">
+                    Construyendo <br>
+                    <strong>Carreras</strong>
+                </div>
+            </div>
+            <?php endif; ?>
+
+            <!-- Ícono de hamburguesa -->
+            <div class="hamburger-menu" onclick="toggleMenu()">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+
+            <nav class="menu-web">
+                <?php wp_nav_menu( array ( 'menu' => 'Header' ) ); ?>
+            </nav>
+        </div>
+        <nav class="menu-wrapper">
+            <?php wp_nav_menu( array ( 'menu' => 'Header' ) ); ?>
+        </nav>
+    </div>
 </header>
 <div class="main-content">
+
+<script>
+    function toggleMenu() {
+        document.querySelector('.menu-wrapper').classList.toggle('active');
+    }
+</script>
